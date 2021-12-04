40c75ee0c164a3b217ea2528f4229583339b1e6f
Austin Auger <a.ustin.mark.auger@gmail.com>
2021-12-03 20:58:09 -0500

    Added test application & screenshot
    Added test example + screenshot for readme

--- TestApp/Form1.Designer.cs
+++ TestApp/Form1.Designer.cs
@@ -28,19 +28,219 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.threed64Panel1 = new Threed64.Threed64Panel();
+            this.threed64GroupBox1 = new Threed64.Threed64GroupBox();
+            this.threed64GroupBox2 = new Threed64.Threed64GroupBox();
+            this.threed64Button1 = new Threed64.Threed64Button();
+            this.threed64Button2 = new Threed64.Threed64Button();
+            this.threed64Button3 = new Threed64.Threed64Button();
+            this.threed64Button4 = new Threed64.Threed64Button();
+            this.threed64Button5 = new Threed64.Threed64Button();
+            this.threed64Button6 = new Threed64.Threed64Button();
+            this.threed64Panel1.SuspendLayout();
+            this.threed64GroupBox1.SuspendLayout();
+            this.threed64GroupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // threed64Panel1
+            // 
+            this.threed64Panel1.AAHighColorSet = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(220)))), ((int)(((byte)(220))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(120)))), ((int)(((byte)(120)))))};
+            this.threed64Panel1.AALowColorSet = new System.Drawing.Color[] {
+        System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245))))),
+        System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))))};
+            this.threed64Panel1.Controls.Add(this.threed64GroupBox2);
+            this.threed64Panel1.Controls.Add(this.threed64GroupBox1);
+            this.threed64Panel1.Location = new System.Drawing.Point(12, 12);
+            this.threed64Panel1.Name = "threed64Panel1";
+            this.threed64Panel1.Size = new System.Drawing.Size(455, 199);
+            this.threed64Panel1.TabIndex = 0;
+            // 
+            // threed64GroupBox1
+            // 
+            this.threed64GroupBox1.AABackColor = System.Drawing.SystemColors.Control;
+            this.threed64GroupBox1.AABorderStep = 2;
+            this.threed64GroupBox1.AABorderStyle = Threed64.Threed64GroupBox.BorderStyle.SINGLE;
+            this.threed64GroupBox1.AADoubleLineColor = System.Drawing.Color.Black;
+            this.threed64GroupBox1.AALeftBuffer = 15;
+            this.threed64GroupBox1.AALineColor = System.Drawing.Color.Black;
+            this.threed64GroupBox1.AAStringPad = 5;
+            this.threed64GroupBox1.AAText = "Text";
+            this.threed64GroupBox1.Controls.Add(this.threed64Button3);
+            this.threed64GroupBox1.Controls.Add(this.threed64Button2);
+            this.threed64GroupBox1.Controls.Add(this.threed64Button1);
+            this.threed64GroupBox1.Location = new System.Drawing.Point(19, 15);
+            this.threed64GroupBox1.Name = "threed64GroupBox1";
+            this.threed64GroupBox1.Size = new System.Drawing.Size(200, 173);
+            this.threed64GroupBox1.TabIndex = 0;
+            this.threed64GroupBox1.TabStop = false;
+            this.threed64GroupBox1.Text = "threed64GroupBox1";
+            // 
+            // threed64GroupBox2
+            // 
+            this.threed64GroupBox2.AABackColor = System.Drawing.SystemColors.Control;
+            this.threed64GroupBox2.AABorderStep = 2;
+            this.threed64GroupBox2.AABorderStyle = Threed64.Threed64GroupBox.BorderStyle.SINGLE;
+            this.threed64GroupBox2.AADoubleLineColor = System.Drawing.Color.Black;
+            this.threed64GroupBox2.AALeftBuffer = 15;
+            this.threed64GroupBox2.AALineColor = System.Drawing.Color.Black;
+            this.threed64GroupBox2.AAStringPad = 5;
+            this.threed64GroupBox2.AAText = "Text";
+            this.threed64GroupBox2.Controls.Add(this.threed64Button6);
+            this.threed64GroupBox2.Controls.Add(this.threed64Button5);
+            this.threed64GroupBox2.Controls.Add(this.threed64Button4);
+            this.threed64GroupBox2.Location = new System.Drawing.Point(236, 15);
+            this.threed64GroupBox2.Name = "threed64GroupBox2";
+            this.threed64GroupBox2.Size = new System.Drawing.Size(200, 173);
+            this.threed64GroupBox2.TabIndex = 0;
+            this.threed64GroupBox2.TabStop = false;
+            this.threed64GroupBox2.Text = "threed64GroupBox1";
+            // 
+            // threed64Button1
+            // 
+            this.threed64Button1.AABaseButtonColor = System.Drawing.SystemColors.Control;
+            this.threed64Button1.AABaseButtonDownColor = System.Drawing.SystemColors.ScrollBar;
+            this.threed64Button1.AABorderWidth = 5;
+            this.threed64Button1.AABottomShadowColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.threed64Button1.AAClickable = true;
+            this.threed64Button1.AAHighlightShadowColor = System.Drawing.SystemColors.ControlLightLight;
+            this.threed64Button1.AAIconImage = ((System.Drawing.Image)(resources.GetObject("threed64Button1.AAIconImage")));
+            this.threed64Button1.AAImageMode = Threed64.Threed64Button.ImageMode.TEXT;
+            this.threed64Button1.AAImageTextPxBuffer = 15;
+            this.threed64Button1.AALineBorderColor = System.Drawing.Color.Black;
+            this.threed64Button1.AALineBorderWidth = 2;
+            this.threed64Button1.AATextString = "Text";
+            this.threed64Button1.Location = new System.Drawing.Point(41, 22);
+            this.threed64Button1.Name = "threed64Button1";
+            this.threed64Button1.Size = new System.Drawing.Size(124, 40);
+            this.threed64Button1.TabIndex = 0;
+            // 
+            // threed64Button2
+            // 
+            this.threed64Button2.AABaseButtonColor = System.Drawing.SystemColors.Control;
+            this.threed64Button2.AABaseButtonDownColor = System.Drawing.SystemColors.ScrollBar;
+            this.threed64Button2.AABorderWidth = 5;
+            this.threed64Button2.AABottomShadowColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.threed64Button2.AAClickable = true;
+            this.threed64Button2.AAHighlightShadowColor = System.Drawing.SystemColors.ControlLightLight;
+            this.threed64Button2.AAIconImage = ((System.Drawing.Image)(resources.GetObject("threed64Button2.AAIconImage")));
+            this.threed64Button2.AAImageMode = Threed64.Threed64Button.ImageMode.TEXT;
+            this.threed64Button2.AAImageTextPxBuffer = 15;
+            this.threed64Button2.AALineBorderColor = System.Drawing.Color.Black;
+            this.threed64Button2.AALineBorderWidth = 2;
+            this.threed64Button2.AATextString = "Text";
+            this.threed64Button2.Location = new System.Drawing.Point(41, 68);
+            this.threed64Button2.Name = "threed64Button2";
+            this.threed64Button2.Size = new System.Drawing.Size(124, 40);
+            this.threed64Button2.TabIndex = 0;
+            // 
+            // threed64Button3
+            // 
+            this.threed64Button3.AABaseButtonColor = System.Drawing.SystemColors.Control;
+            this.threed64Button3.AABaseButtonDownColor = System.Drawing.SystemColors.ScrollBar;
+            this.threed64Button3.AABorderWidth = 5;
+            this.threed64Button3.AABottomShadowColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.threed64Button3.AAClickable = true;
+            this.threed64Button3.AAHighlightShadowColor = System.Drawing.SystemColors.ControlLightLight;
+            this.threed64Button3.AAIconImage = ((System.Drawing.Image)(resources.GetObject("threed64Button3.AAIconImage")));
+            this.threed64Button3.AAImageMode = Threed64.Threed64Button.ImageMode.TEXT;
+            this.threed64Button3.AAImageTextPxBuffer = 15;
+            this.threed64Button3.AALineBorderColor = System.Drawing.Color.Black;
+            this.threed64Button3.AALineBorderWidth = 2;
+            this.threed64Button3.AATextString = "Text";
+            this.threed64Button3.Location = new System.Drawing.Point(41, 114);
+            this.threed64Button3.Name = "threed64Button3";
+            this.threed64Button3.Size = new System.Drawing.Size(124, 40);
+            this.threed64Button3.TabIndex = 0;
+            // 
+            // threed64Button4
+            // 
+            this.threed64Button4.AABaseButtonColor = System.Drawing.SystemColors.Control;
+            this.threed64Button4.AABaseButtonDownColor = System.Drawing.SystemColors.ScrollBar;
+            this.threed64Button4.AABorderWidth = 5;
+            this.threed64Button4.AABottomShadowColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.threed64Button4.AAClickable = true;
+            this.threed64Button4.AAHighlightShadowColor = System.Drawing.SystemColors.ControlLightLight;
+            this.threed64Button4.AAIconImage = ((System.Drawing.Image)(resources.GetObject("threed64Button4.AAIconImage")));
+            this.threed64Button4.AAImageMode = Threed64.Threed64Button.ImageMode.TEXT;
+            this.threed64Button4.AAImageTextPxBuffer = 15;
+            this.threed64Button4.AALineBorderColor = System.Drawing.Color.Black;
+            this.threed64Button4.AALineBorderWidth = 2;
+            this.threed64Button4.AATextString = "Text";
+            this.threed64Button4.Location = new System.Drawing.Point(40, 22);
+            this.threed64Button4.Name = "threed64Button4";
+            this.threed64Button4.Size = new System.Drawing.Size(124, 40);
+            this.threed64Button4.TabIndex = 0;
+            // 
+            // threed64Button5
+            // 
+            this.threed64Button5.AABaseButtonColor = System.Drawing.SystemColors.Control;
+            this.threed64Button5.AABaseButtonDownColor = System.Drawing.SystemColors.ScrollBar;
+            this.threed64Button5.AABorderWidth = 5;
+            this.threed64Button5.AABottomShadowColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.threed64Button5.AAClickable = true;
+            this.threed64Button5.AAHighlightShadowColor = System.Drawing.SystemColors.ControlLightLight;
+            this.threed64Button5.AAIconImage = ((System.Drawing.Image)(resources.GetObject("threed64Button5.AAIconImage")));
+            this.threed64Button5.AAImageMode = Threed64.Threed64Button.ImageMode.TEXT;
+            this.threed64Button5.AAImageTextPxBuffer = 15;
+            this.threed64Button5.AALineBorderColor = System.Drawing.Color.Black;
+            this.threed64Button5.AALineBorderWidth = 2;
+            this.threed64Button5.AATextString = "Text";
+            this.threed64Button5.Location = new System.Drawing.Point(40, 68);
+            this.threed64Button5.Name = "threed64Button5";
+            this.threed64Button5.Size = new System.Drawing.Size(124, 40);
+            this.threed64Button5.TabIndex = 0;
+            // 
+            // threed64Button6
+            // 
+            this.threed64Button6.AABaseButtonColor = System.Drawing.SystemColors.Control;
+            this.threed64Button6.AABaseButtonDownColor = System.Drawing.SystemColors.ScrollBar;
+            this.threed64Button6.AABorderWidth = 5;
+            this.threed64Button6.AABottomShadowColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.threed64Button6.AAClickable = true;
+            this.threed64Button6.AAHighlightShadowColor = System.Drawing.SystemColors.ControlLightLight;
+            this.threed64Button6.AAIconImage = ((System.Drawing.Image)(resources.GetObject("threed64Button6.AAIconImage")));
+            this.threed64Button6.AAImageMode = Threed64.Threed64Button.ImageMode.TEXT;
+            this.threed64Button6.AAImageTextPxBuffer = 15;
+            this.threed64Button6.AALineBorderColor = System.Drawing.Color.Black;
+            this.threed64Button6.AALineBorderWidth = 2;
+            this.threed64Button6.AATextString = "Text";
+            this.threed64Button6.Location = new System.Drawing.Point(40, 114);
+            this.threed64Button6.Name = "threed64Button6";
+            this.threed64Button6.Size = new System.Drawing.Size(124, 40);
+            this.threed64Button6.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(527, 521);
+            this.ClientSize = new System.Drawing.Size(481, 223);
+            this.Controls.Add(this.threed64Panel1);
             this.Name = "Form1";
             this.Text = ".NET 6.0 Threed64";
+            this.threed64Panel1.ResumeLayout(false);
+            this.threed64GroupBox1.ResumeLayout(false);
+            this.threed64GroupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Threed64.Threed64Panel threed64Panel1;
+        private Threed64.Threed64GroupBox threed64GroupBox2;
+        private Threed64.Threed64Button threed64Button6;
+        private Threed64.Threed64Button threed64Button5;
+        private Threed64.Threed64Button threed64Button4;
+        private Threed64.Threed64GroupBox threed64GroupBox1;
+        private Threed64.Threed64Button threed64Button3;
+        private Threed64.Threed64Button threed64Button2;
+        private Threed64.Threed64Button threed64Button1;
     }
 }
